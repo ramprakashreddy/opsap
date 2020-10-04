8c7973247c870114b8e8be7ee09bc5b2d8371c0f
ramprakashreddy <rpr1612@gmail.com>
2020-10-04 20:46:30 +0530

    PROJECT COMPLETED

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
+//Project By Ram Prakash Reddy
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import Navigation from './src/navigation/Navigator';
+import { loadAsync } from 'expo-font';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontLoaded: false
+    }
+  }
+  async componentDidMount() {
+    await loadAsync({
+      "NeueHaasDisplay-Light": require("./assets/fonts/NeueHaasDisplay-Light.ttf"),
+      "NeueHaasDisplay-Medium": require("./assets/fonts/NeueHaasDisplay-Mediu.ttf"),
+      "NeueHaasDisplay-Bold": require("./assets/fonts/NeueHaasDisplay-Bold.ttf"),
+    })
+    this.setState({
+      fontLoaded: true
+    })
+  }
+
+  render() {
+    if (this.state.fontLoaded)
+      return (
+        <Navigation />
+      );
+    else return (
+      <View>
+        <ActivityIndicator size='large' />
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
